cb74ac756ae4d7b9938420c69540e24910827045
alana_eloeva <alana.eloeva@bk.ru>
2017-11-12 19:55:34 +0300

    optimized iphone 4s

--- GameTimeKill/ViewController.swift
+++ GameTimeKill/ViewController.swift
@@ -306,8 +306,8 @@ extension String{
 }
 
 extension Date {
-    var millisecondsSince1970:Int {
-        return Int((self.timeIntervalSince1970 * 1000.0).rounded())
+    var millisecondsSince1970:Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
     }
     
     init(milliseconds:Int) {
